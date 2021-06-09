38baf3f54ee0734e1e42de1207c1155218a2b3ba
Noak Pettersson <noake12@gmail.com>
2021-06-09 11:35:56 +0200

    grid resizing done

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const container = document.querySelector('#container');
+const clearBtn = document.querySelector('#clearBtn');
 
-function createGrid(rows, colums) {
-    for (let i = 0; i < rows * colums; i++) {
+// Creates the grid
+//function createGrid(rows, colums) {
+function createGrid(squares) {
+    for (let i = 0; i < (squares * squares); i++) {
         const div = document.createElement('div');
         div.classList.toggle('cell');
         container.appendChild(div);
     }
 
+    // Add eventlisteners to the cells in the grid
     var gridCells = document.querySelectorAll('.cell');
     gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
-
 }
 
+function sizingGrid(squares){
+    container.style.setProperty('--grid-rows', squares );
+    container.style.setProperty('--grid-columns', squares);
+}
+
+function delGrid() {
+    container.innerHTML = "";
+  }
+
+
 function changeColor() {
-
     this.style.backgroundColor = randomColor();
-
 }
 
 function randomColor() {
@@ -30,14 +41,35 @@ function randomColor() {
 
 function clear() {
 
+    // get all the cells and set their color to
+    // the "default one"
     var gridCells = document.querySelectorAll('.cell');
     gridCells.forEach(gridPixel => gridPixel.style.backgroundColor = "rgba(255, 255, 255, 0.865)");
 
-    let res = prompt("How any : ");
-    alert(res);
+    // ask for the size of the new grid 
+    // and create it
+    let res = prompt("How many squares on each side (max 100, min 1)? ");
+    if(res > 100 || res <= 0)
+        res = 16;
+    
+    delGrid();
+    sizingGrid(res);
+    createGrid(res);
+    
+    
 }
 
-const clearBtn = document.getElementById('clearBtn');
-clearBtn.addEventListener('click', clear);
+function resizeGrid(squares){
+    sizingGrid(squares);
+    createGrid(squares);
+}
 
-createGrid(16, 16);
+
+
+// Default size
+sizingGrid(16);
+createGrid(16);
+
+// Event listeners
+clearBtn.addEventListener('click', clear);
+//clearBtn.addEventListener('click', resizeGrid);
